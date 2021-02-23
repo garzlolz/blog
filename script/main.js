f0f01820db4d681276af3ada77618ea1eb7401b3
garz <oscar861213@gmail.com>
2021-02-23 20:31:01 +0800

    新增貼文

--- script/main.js
+++ script/main.js
@@ -2,5 +2,6 @@ alert('歡迎')
 
 function gett(){
     var name = document.getElementById('name').value;
+    if(name == 0){return};
     document.getElementById('change').textContent = name + '，你好!';
 }
