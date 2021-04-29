8463c82bb1b0371afaa1c30c23814a39918605c2
施宏勳 <garz@shifuxunde-MacBook-Pro.local>
2021-04-29 12:01:00 +0800

    Update main.js

--- script/main.js
+++ script/main.js
@@ -94,9 +94,28 @@ var data=[
     {
         date:'3月17號 ,2021 , ',
         title:'關於AJAX',
-        content:'今天是很熱的一天，下午2點時到數唯科技面試，聽了工作內容令我有點映象深刻，他們的主管跟我說像我這種剛畢業的人培訓是必要的，對於我這種剛畢業只懂一點前端的，他們可以培訓，比如說前端的angular還有.net以及C#，這些對我來說都蠻新鮮的；<br><br>面試結束後，騎了老遠到家想說剪個頭法，第一間面試的勁峰就打給我問我要不要上班；有點掙扎，因為相比這一間就是比較傳統的公司、MIS工程師實際上是在幹嘛也聽不太懂<br><br>回到正題，昨天練習的xmlhttprequest<br>總之先請求連線<br><br>" var xhr = new XMLhttpRequest()"<br><br>接下來請求URL<br><br>"xhr.open("post/get","url",true)"'+
-        '<br><br>"xhr.send(null) // 若沒有要傳送資料"<br><br>'+
-        '最後是 "xhr.onload=function(){執行動作}"'
+        content:`<pre>
+        今天是很熱的一天，下午2點時到數唯科技面試，聽了工作內容令我有點映象深刻，
+        他們的主管跟我說像我這種剛畢業的人培訓是必要的，對於我這種剛畢業只懂一點前端的，
+        他們可以培訓，比如說前端的angular還有.net以及C#，這些對我來說都蠻新鮮的；
+        面試結束後，騎了老遠到家想說剪個頭法，第一間面試的勁峰就打給我問我要不要上班；
+        
+            有點掙扎，因為相比這一間就是比較傳統的公司、MIS工程師實際上是在幹嘛也聽不太懂
+        回到正題，昨天練習的xmlhttprequest
+        
+        //總之先請求連線
+         var xhr = new XMLhttpRequest();
+
+        //接下來請求URL
+        xhr.open("post/get","url",true);
+
+        //若沒有要傳送資料"
+        xhr.send(null)
+         
+        //最後是
+        xhr.onload=function(){執行動作}
+        </pre>
+        `
     }
     ,
     {
